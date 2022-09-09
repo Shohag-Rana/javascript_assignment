246c9dcf8cf9da1841236de3d1ba51925f167081
Shohag-Rana <mdshohag7613@gmail.com>
2022-09-09 11:27:11 +0600

    footer display change

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,5 @@
 var preloader = document.getElementById("loading");
 function preLoadingFunction(){
     preloader.style.display = 'none';
-    document.getElementsByClassName('footer').display = none;
+    document.getElementsByClassName('footer').display = "none";
 }
